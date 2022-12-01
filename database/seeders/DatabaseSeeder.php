73a0a14e4f1e63636dca6aad0d0a334725676ea3
lightdevkimo <karim.ranim90@gmail.com>
2022-12-01 23:50:53 +0200

    seeders done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $role1 = Role::create(['name' => 'admin']);
+        $role2 = Role::create(['name' => 'employer']);
+
+        $admin = \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'password'=> bcrypt('admin')
+        ]);
+
+        $employer = \App\Models\User::factory()->create([
+            'name' => 'Employer',
+            'email' => 'employer@test.com',
+            'password'=> bcrypt('employer')
+        ]);
+
+        $admin->assignRole($role1);
+        $employer->assignRole($role2);
+
     }
 }
